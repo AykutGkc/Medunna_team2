886a5360b3eb9f678bf49950642be39c61ac1bf3
Aykut Gökce <aykutgokce2016@gmail.com>
2024-02-03 13:01:09 +0100

    aykut_sp

---
+++ src/test/java/pages/A01SatirPaylasimi.java
@@ -0,0 +1,44 @@
+package pages;
+
+public class A01SatirPaylasimi {
+
+    //-----Satir Araliklari------
+    //İlyas K.      --->   20-70
+    //Gokhan K.     --->   71-120
+    //Yusuf H.      --->   121-170
+    //Aykut G.      --->   171-220
+    //Mustafa B.    --->   221-270
+    //Alı İ.        --->   271-320
+    //Sefa T        --->   321-370
+    //Esra C.       --->   371-421
+    //Fethullah K.  --->   422-472
+    //Omer Faruk C. --->  473-522
+
+
+/*
+
+    !!!!Dikkat!!!!
+    ---Lütfen kendi branch'ınızda işiniz tamamlandığında github'a push etmeden önce methodunuz için ayrılan alanın
+    Satir paylasimi class'taki ve configuration.propertiees dosyasinda ki alan ile uyumlu olup olmadığını kontrol ediniz.
+    ---Alan yeterli değilse alana uygun bir kod yazınız. Alanın artırılması talebinizi Github sorumlusuna iletiniz.
+    ---pull yaptiktan sonra size verilen alan dogru degilse bos satir silerek yada atlayarak(enter) ilgili satira geliniz
+
+
+    ----GitHub---
+
+    Projeyi ilk kez intellije cekme(clone)
+    --file->new-->Project from version Control->URL:   yapistiriniz. ->clone butonuna tiklayiniz.
+    --Kendi branch'inizi olusturunuz. (git branch isminiz)
+
+    Normal calisma günlerinde;
+    1-Localinizde Master branch'a geciniz. (git checkout master)
+    2-Master brach da iken projeyi pull ediniz. (git pull)
+    3-Kendi brach'iza geciniz. (kendi brach'iniza gecmek icin--> git checkout branchIsmi
+    4-Master brach'ini kendi branch'iniaza cekiniz. (git merge master)
+    5-Kendi branch'inizda calisip, git add . ve git commit -m"VersiyonAdiniz" yapiniz.
+    6-!!!KOD ARALIGININ SIZE VERILEN ARALIK OLDUGUNA DIKKAT EDINIZ!!!
+    7-Kendi branch'inizi push ediniz. (git push)
+
+ */
+
+}
